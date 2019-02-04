bf6d719f9c5d419cc628b897378120852b964449
Edric <1802320@sit.singaporetech.edu.sg>
2019-02-04 13:31:45 +0800

    Reader Done

--- OSv0.c
+++ OSv0.c
@@ -3,12 +3,10 @@
 #include <stdlib.h>
 
 int main(){
-    char buf[1024];
-    FILE *read = fopen("filename","r");
-  
-   while (fgets(buf, 1024, read){
-       char* tmp = strdup(line);
-       printf("Field 3 would be %s\n", getfield(tmp, 3));
-       free(tmp);
+    FILE *fp = fopen("Test.csv","r");
+    char line[1024];
+
+    while (fgets(line, 1024, fp)){
+        printf(line);
     }
 }
