c4122d778bdc9487a47908701908d05f0e6220cc
Edric <1802320@sit.singaporetech.edu.sg>
2019-02-07 09:21:42 +0800

    Commits from Eunice to be pushed

--- OSv0.c
+++ OSv0.c
@@ -1,12 +1,41 @@
+//@@ -1,40 +0,0 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-int main(){
-    FILE *fp = fopen("Test.csv","r");
-    char line[1024];
+int main() {
+	// open file pointer
+	FILE *fp = fopen("Test.csv", "r");
+	char line[1024];
 
-    while (fgets(line, 1024, fp)){
-        printf(line);
-    }
+	// read the file content
+	// add, 100, 101, 102, 103, 104, 105, 106 ---> token will be add then 100 then 101 ...
+	// add, 200, 201, 202, 203
+	// read, 106
+	// delete, 200
+	// we need to use string tokenizer to read comma-separated text
+	while (fgets(line, 1024, fp)) {
+		printf(line);
+		
+		char separator[2] = ",";
+		char * token;
+
+		// get the first token
+		// token will be add
+		token = strtok(line, separator); // s will be 100, 101, 102, ... 
+
+		// walk through other tokens
+		while(token != NULL)
+		{
+			printf("%s \n", token);
+
+			// this is to read next token, token will be 100 and s will be 101, 102, ...
+			// btw, file identifiers : 100, 200, 300 etc
+			// file contents : 101, 102, 201, 202 .. etc
+			token = strtok(NULL, separator);
+		}
+	}
+
+	fclose(fp);
+
 }
